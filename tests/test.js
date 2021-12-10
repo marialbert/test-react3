1877cbd9544aebd187ff210454343a0a99ec814e
Marialbert <marialbertflores4@gmail.com>
2021-12-10 15:26:16 -0400

    cambio color de boton

--- tests/test.js
+++ tests/test.js
@@ -7,5 +7,8 @@ fixture('MyFixture')
 
 test('test', async t => {
   await t.typeText('#root .TodoSearch', 'Mabe test');
+  await t.click(Selector('#root button').withText('+'))
+  await t.typeText('#modal div form textarea', 'test')
+  await t.click(Selector('#modal button').withText('Añadir'))
   await percySnapshot(t, 'TestCafe Example');
 });
