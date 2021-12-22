06c877ac80ab05f28d3d33f8a2839f30455e2de2
Marialbert <marialbertflores4@gmail.com>
2021-12-22 16:17:13 -0400

    test 26

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,11 @@ import { Selector } from 'testcafe';
 import percySnapshot from '@percy/testcafe';
 
 fixture('MyFixture')
-    .page('http://192.168.0.196:3000/test-react3');
+    .page('http://localhost:3000/test-react3');
   //.page('https://marialbert.github.io/test-react3/');
 
 test('test', async t => {
-  await t.typeText('#root .TodoSearch', 'Mabe test');
+  //await t.typeText('#root .TodoSearch', 'Mabe test');
   await t.click(Selector('#root button').withText('+'))
   await t.typeText('#modal div form textarea', 'test')
   await t.click(Selector('#modal button').withText('Añadir'))
