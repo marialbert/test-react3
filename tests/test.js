27cc7bcf1a1c090ed98491421aa1798e49b8dec7
Marialbert <marialbertflores4@gmail.com>
2021-12-22 16:02:23 -0400

    test 24

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 import percySnapshot from '@percy/testcafe';
 
 fixture('MyFixture')
-  .page('./');
+  .page('https://marialbert.github.io/test-react3/');
 
 test('test', async t => {
   await t.typeText('#root .TodoSearch', 'Mabe test');
