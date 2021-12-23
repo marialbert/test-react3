53c6ebe83ab82d598627c73536a92fc0c9330676
Marialbert <marialbertflores4@gmail.com>
2021-12-23 14:39:40 -0400

    test 26

--- tests/test.js
+++ tests/test.js
@@ -5,12 +5,13 @@ fixture('MyFixture')
     .page('http://localhost:3000/test-react3');
   //.page('https://marialbert.github.io/test-react3/');
 
+test('test', async t => {
+
   await runner
   .browsers('chrome')
   .src('./tests/')
   .run({ quarantineMode: true }); 
 
-test('test', async t => {
   await t.typeText('#root .TodoSearch', 'Mabe test');
   await t.click(Selector('#root button').withText('+'))
   await t.typeText('#modal div form textarea', 'test')
