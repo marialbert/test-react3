63cff709df280e424f5a952c22fbee36e52c1d27
Marialbert <marialbertflores4@gmail.com>
2021-12-27 16:19:53 -0400

    test 27

--- tests/test.js
+++ tests/test.js
@@ -2,15 +2,10 @@ import { Selector } from 'testcafe';
 import percySnapshot from '@percy/testcafe';
 
 fixture('MyFixture')
-    .page('http://localhost:3000/test-react3');
-  //.page('https://marialbert.github.io/test-react3/');
+  //.page('http://localhost:3000/test-react3');
+  .page('https://marialbert.github.io/test-react3/');
 
 test('test', async t => {
-
-  await runner
-  .browsers('chrome')
-  .src('./tests/')
-  .run({ quarantineMode: true }); 
 
   await t.typeText('#root .TodoSearch', 'Mabe test');
   await t.click(Selector('#root button').withText('+'))
