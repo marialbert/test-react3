6b148d454cfc66254542d06dceefb821696b45f4
Marialbert <marialbertflores4@gmail.com>
2021-12-30 12:34:58 -0400

    test 29

--- tests/test.js
+++ tests/test.js
@@ -5,9 +5,9 @@ import percySnapshot from '@percy/testcafe';
 //const url='http://localhost:3000/test-react3'
 
 fixture('MyFixture')
-  //.page('https://github.com/marialbert/test-react3.git');
+     .page('http://192.168.0.196:3000/test-react3');
   // .page('http://localhost:3000/test-react3');
-    .page('https://marialbert.github.io/test-react3/');
+  //.page('https://marialbert.github.io/test-react3/');
 
 test('test', async t => {
 
