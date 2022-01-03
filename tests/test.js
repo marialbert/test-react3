da5480db6ee6624fc3597be93ab1f3a82ce14f7b
Marialbert <marialbertflores4@gmail.com>
2022-01-03 10:02:39 -0400

    test 5

--- tests/test.js
+++ tests/test.js
@@ -23,8 +23,5 @@ fixture `react example`
     .page('http://localhost:3000/test-react3')
 
 test('test', async t => {
-    await t.click(Selector('#root button').withText('+'))
-    await t.typeText('#modal div form textarea', 'test')
-    await t.click(Selector('#modal button').withText('Añadir'))
-    await percySnapshot(t, 'TestCafe Example');
+    console.log ("test react")
 });
