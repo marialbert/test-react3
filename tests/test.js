0b6d5ed63994313b4d347e8824bcbabfeb43236e
Marialbert <marialbertflores4@gmail.com>
2022-01-03 09:51:56 -0400

    test 5

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,10 @@ import { Selector } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 import percySnapshot from '@percy/testcafe';
 
+
 //const url='http://localhost:3000/test-react3'
 
-fixture('MyFixture')
+/*fixture('MyFixture')
    // .page('http://192.168.0.196:3000/test-react3');
   //  .page('http://localhost:3000/test-react3');
      .page('https://marialbert.github.io/test-react3/');
@@ -16,4 +17,14 @@ test('test', async t => {
   await t.typeText('#modal div form textarea', 'test')
   await t.click(Selector('#modal button').withText('Añadir'))
   await percySnapshot(t, 'TestCafe Example');
+});*/
+
+fixture `react example`
+    .page('http://localhost:3000/test-react3')
+
+test('test', async t => {
+    await t.click(Selector('#root button').withText('+'))
+    await t.typeText('#modal div form textarea', 'test')
+    await t.click(Selector('#modal button').withText('Añadir'))
+    await percySnapshot(t, 'TestCafe Example');
 });
