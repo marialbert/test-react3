88be46f3f0d1caca3e4fa989abcd06a1edbe8f61
Marialbert <marialbertflores4@gmail.com>
2022-01-04 10:28:29 -0400

    test local

--- tests/test.js
+++ tests/test.js
@@ -7,12 +7,12 @@ import { userVariables } from 'testcafe';
 //const url='http://localhost:3000/test-react3'
 
 fixture('MyFixture')
-      // .page('http://localhost:3000/test-react3');
-     .page('https://marialbert.github.io/test-react3/');
+    .page('http://localhost:3000/test-react3');
+     //.page('https://marialbert.github.io/test-react3/');
 
 test('test', async t => {
 
-  await t.typeText('#root .TodoSearch', 'Mabe test',{ timeout: 500 });
+  await t.typeText('#root .TodoSearch', 'Mabe test');
   await t.click(Selector('#root button').withText('+'))
   await t.typeText('#modal div form textarea', 'test')
   await t.click(Selector('#modal button').withText('Añadir'))
